0b14eb77e21bdedada2c128dd42e6e8c079f72bb
shahanas7210 <shahanas7210@gmail.com>
2025-03-20 23:03:32 +0530

    df

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 const nextConfig = {
+    output:"export",
     images: {
         domains: ["placehold.co"],
         dangerouslyAllowSVG: true,
-        contentSecurityPolicy: "default-src 'self'; img-src *; media-src 'none'; script-src 'none'; sandbox;",
+        // contentSecurityPolicy: "default-src 'self'; img-src *; media-src 'none'; script-src 'none'; sandbox;",
     },
    
 };
