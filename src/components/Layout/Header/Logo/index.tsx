accec521b34f9157eaef2759ad9e5fd5b9762041
shahanas7210 <shahanas7210@gmail.com>
2025-03-22 14:11:45 +0530

    logo change 2

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -1,31 +1,15 @@
-import { color } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
+import { color } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Logo: React.FC = () => {
-  const logoVariable = [
-    {
-      text: "c",
-      color: "#006cb0",
-      colorText: "text-white",
-    },
-    {
-      text: "e",
-      color: "#8f2b8c",
-      colorText: "text-white",
-    },
-    {
-      text: "o",
-      color: "#29a084",
-      colorText: "text-white",
-    },
-  ];
+
+  const logoVariable=[{
+    text:"s"
+  }]
 
   return (
-    <Link
-      href="/"
-      className="flex items-center text-black dark:text-white text-2xl font-semibold gap-4"
-    >
+    <Link href="/" className='flex items-center text-black dark:text-white text-2xl font-semibold gap-4'>
       {/* <Image
         src="/images/logo/logo.png"
         alt="logo"
@@ -34,21 +18,22 @@ const Logo: React.FC = () => {
         // style={{ width: 'auto', height: 'auto' }}
         quality={100}
       /> */}
-      <div className=" text-center   gap-4  w-40 h-16">
-        <h1 className="text-[#bb8528]">SMART</h1>
-        <div className="flex justify-between ">
-          {logoVariable.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className={`bg-[${item.color}] ${item.colorText} size-10 flex items-center justify-center `}
-              >
-                <h1 className="capitalize">{item.text}</h1>
-              </div>
-            );
-          })}
+      <div className=' text-center   gap-4  w-40 h-16'>
+        <h1 className='text-[#bb8528]'>SMART</h1>
+        <div className='flex justify-between '>
+        <div className='bg-[#006cb0] size-10  '>
+          <h1 className='text-white'>C</h1>
         </div>
+        <div className='bg-[#8f2b8c] size-10 '>
+        <h1 className='text-white'>E</h1>
+        </div>
+        <div className='bg-[#29a084] size-10 flex items-center justify-center '>
+        <h1 className='text-white'>O</h1>
+        </div>
+        </div>
+       
       </div>
+      
     </Link>
   );
 };
